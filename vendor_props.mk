56367cf7fa1debdfa1e5ff807579575c306f2a76
Ali B <abittin@gmail.com>
2020-04-06 14:40:04 +0300

    guacamoleb: add missing vendor display properties
    Change-Id: I6ffca7742318e5fef6a1b09c680ec347b721872b

--- vendor_props.mk
+++ vendor_props.mk
@@ -6,6 +6,21 @@
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.display.ad.hdr_calib_data=/vendor/etc/hdr_config.cfg \
+    ro.vendor.display.ad.sdr_calib_data=/vendor/etc/sdr_config.cfg \
+    ro.vendor.display.ad=1 \
+    ro.vendor.display.cabl=2 \
+    ro.vendor.display.sensortype=2 \
+    persist.vendor.color.matrix=2 \
+    vendor.display.comp_mask=0 \
+    vendor.display.dataspace_saturation_matrix=1.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,1.0 \
+    vendor.display.disable_decimation=1 \
+    vendor.display.disable_excl_rect=0 \
+    vendor.display.disable_hw_recovery=0 \
+    vendor.display.disable_hw_recovery_dump=1 \
+    vendor.display.disable_inline_rotator=1 \
+    vendor.display.disable_scaler=0 \
+    vendor.display.enable_null_display=0 \
     vendor.display.enable_optimize_refresh=1 \
     ro.sf.lcd_density=420 \
     vendor.display.disable_mask_layer_hint=1
